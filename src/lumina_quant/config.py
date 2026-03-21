b5a18852c1ed96b652ae0e04ebcd90f390b99f43
hoky1227 <hoky1227@gmail.com>
2026-03-21 14:17:02 +0900

    omx(team): auto-checkpoint worker-1 [unknown]

--- src/lumina_quant/config.py
+++ src/lumina_quant/config.py
@@ -2,18 +2,24 @@
 
 from __future__ import annotations
 
+import importlib
 import os
+import sys
 
 from lumina_quant.configuration.loader import load_yaml_config
-from lumina_quant.configuration.runtime_access import (
-    BaseConfig,
-    BacktestConfig,
-    LiveConfig,
-    OptimizationConfig,
-    current_market_data_runtime_settings,
-    export_runtime_dict,
-    load_current_runtime_config,
-)
+_runtime_access_module_name = "lumina_quant.configuration.runtime_access"
+if _runtime_access_module_name in sys.modules:
+    _runtime_access = importlib.reload(sys.modules[_runtime_access_module_name])
+else:
+    _runtime_access = importlib.import_module(_runtime_access_module_name)
+
+BaseConfig = _runtime_access.BaseConfig
+BacktestConfig = _runtime_access.BacktestConfig
+LiveConfig = _runtime_access.LiveConfig
+OptimizationConfig = _runtime_access.OptimizationConfig
+current_market_data_runtime_settings = _runtime_access.current_market_data_runtime_settings
+export_runtime_dict = _runtime_access.export_runtime_dict
+load_current_runtime_config = _runtime_access.load_current_runtime_config
 
 
 def load_config(config_path: str = "config.yaml") -> dict:
